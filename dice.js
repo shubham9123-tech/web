bab57c9eaca05bc805a682146518af3aaad5ed8e
shubham9123-tech <61152724+shubham9123-tech@users.noreply.github.com>
2021-03-14 05:38:05 -0900

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,13 @@
+var n =Math.floor(Math.random()*6)+1;
+var dice_image= "dice"+n+".png";
+var imagesource = "images/"+ dice_image;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",imagesource);  
+
+var n =Math.floor(Math.random()*6)+1;
+var dice_image= "dice"+n+".png";
+var imagesource = "images/"+ dice_image;
+
+var image1 = document.querySelectorAll("img")[1];
+image1.setAttribute("src",imagesource);
